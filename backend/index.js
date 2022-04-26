4f9356a183363585093f16c53f315823c148f883
Brian Thomas <brianthomas571@gmail.com>
2022-04-25 19:23:02 -0500

    routes updates

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,64 @@
 const express = require("express")
+const morganLogger = require("morgan")
+const mongoose = require("mongoose")
+
+
+const port = process.env.PORT || 3000;
+require('dotenv').config()
 
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Test")
+const api = process.env.API_URL
+
+// middleware
+app.use(express.json())
+app.use(morganLogger("tiny"))
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number
 })
 
+const Product = mongoose.model("Product", productSchema)
 
-app.listen(3000, () => {
-    console.log("Server is running at 3000")
+app.get(`${api}/products`, async (req,res) => {
+    const productList = await Product.find()
+
+    if(!productList){
+        res.status(500).json({
+            success: false
+        })
+    }
+    res.send(productList)
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+
+    product.save().then((product) => {
+        res.status(201).json(product)
+    }).catch((error)=> {
+       res.status(500).json({
+           error: error,
+           success: false
+       })
+    })
+})
+
+mongoose.connect(process.env.DBConnect)
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`)
 })
